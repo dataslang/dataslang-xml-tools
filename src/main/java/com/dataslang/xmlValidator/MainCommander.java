94c790b54342c25c6dfbea0692d8a1448d1f9afa
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-06 17:28:37 +0200

    Fixed assumed param to debug

--- src/main/java/com/dataslang/xmlValidator/MainCommander.java
+++ src/main/java/com/dataslang/xmlValidator/MainCommander.java
@@ -6,6 +6,6 @@ public class MainCommander {
     @Parameter(names = {"--help", "-h"}, help = true, description = "Shows this help")
     public boolean help = false;
 
-    @Parameter(names = {"--debug", "-d"}, description = "Debug mode")
+    @Parameter(names = {"--debug", "-d"}, description = "Debug mode", arity = 0)
     public boolean debug = false;
 }

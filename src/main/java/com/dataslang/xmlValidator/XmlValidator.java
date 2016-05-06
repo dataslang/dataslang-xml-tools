f31d815d09307446be24691fcf7e1d9dec5eb843
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-06 15:36:36 +0200

    added the main and others options

--- src/main/java/com/dataslang/xmlValidator/XmlValidator.java
+++ src/main/java/com/dataslang/xmlValidator/XmlValidator.java
@@ -5,22 +5,27 @@ import com.beust.jcommander.JCommander;
 public class XmlValidator {
 
     public static void main(String[] args){
-        ValCommander val = new ValCommander();
+        MainCommander com = new MainCommander();
         JCommander jct = null;
+
+        ValCommander val = new ValCommander();
+        jct.addCommand("validate", val);
+        XsltCommander xsl = new XsltCommander();
+        jct.addCommand("transform", xsl);
         try {
-            jct = new JCommander(val, args);
+            jct = new JCommander(com, args);
         } catch (Exception e) {
-            if(val.debug)
+            if(com.debug)
                 e.printStackTrace();
             else
                 System.out.println(e.getMessage());
             System.exit(1);
         }
-        if(args.length == 0 || val.help){
+        if(args.length == 0 || com.debug){
             jct.usage();
             System.exit(0);
         }
-        Validator validator = new Validator(val.xml, val.xsd, val.debug);
+        Validator validator = new Validator(val.xml, val.xsd, com.debug);
         validator.validate();
     }
 }

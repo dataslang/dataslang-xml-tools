ac98216b9952499046e4d1c1002802bc122cdb20
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-06 16:08:33 +0200

    Added the missing "Parameters" annotation

--- src/main/java/com/dataslang/xmlValidator/ValCommander.java
+++ src/main/java/com/dataslang/xmlValidator/ValCommander.java
@@ -1,7 +1,9 @@
 package com.dataslang.xmlValidator;
 
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 
+@Parameters(commandDescription = "Validates the two file passed as parameter")
 public class ValCommander {
     @Parameter(names = "--xml", required = true, description = "XML target file")
     public String xml;
--- src/main/java/com/dataslang/xmlValidator/XsltCommander.java
+++ src/main/java/com/dataslang/xmlValidator/XsltCommander.java
@@ -1,7 +1,9 @@
 package com.dataslang.xmlValidator;
 
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 
+@Parameters(commandDescription = "Transform the xml by the logic of the xslt")
 public class XsltCommander {
     @Parameter(names = "--xml", required = true, description = "XML target file")
     public String xml;

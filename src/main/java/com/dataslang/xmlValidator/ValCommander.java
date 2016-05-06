f92f4ff24703b2cd5d8012a92ec302da6bfc20b6
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-06 15:35:46 +0200

    removed generalized parameters

--- src/main/java/com/dataslang/xmlValidator/ValCommander.java
+++ src/main/java/com/dataslang/xmlValidator/ValCommander.java
@@ -3,12 +3,6 @@ package com.dataslang.xmlValidator;
 import com.beust.jcommander.Parameter;
 
 public class ValCommander {
-    @Parameter(names = {"--help", "-h"}, help = true)
-    public boolean help;
-
-    @Parameter(names = {"--debug", "-d"}, description = "Debug mode")
-    public boolean debug = false;
-
     @Parameter(names = "--xml", required = true, description = "XML target file")
     public String xml;
 

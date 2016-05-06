c752dbe608c3325dd4c4ca20ab0a5a4810beea5f
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-06 16:09:29 +0200

    added description to help command

--- src/main/java/com/dataslang/xmlValidator/MainCommander.java
+++ src/main/java/com/dataslang/xmlValidator/MainCommander.java
@@ -3,8 +3,8 @@ package com.dataslang.xmlValidator;
 import com.beust.jcommander.Parameter;
 
 public class MainCommander {
-    @Parameter(names = {"--help", "-h"}, help = true)
-    public boolean help;
+    @Parameter(names = {"--help", "-h"}, help = true, description = "Shows this help")
+    public boolean help = false;
 
     @Parameter(names = {"--debug", "-d"}, description = "Debug mode")
     public boolean debug = false;

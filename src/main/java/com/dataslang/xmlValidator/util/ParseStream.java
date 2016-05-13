0e023031fbcf8837914ee36ab96b4bdd7ba9fa23
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-13 10:51:22 +0200

    added Stream Utility

---
+++ src/main/java/com/dataslang/xmlValidator/util/ParseStream.java
@@ -0,0 +1,117 @@
+package com.dataslang.xmlValidator.util;
+
+import java.io.*;
+
+public class ParseStream {
+
+    public static void inputStreamSaveFile(String input, String output){
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            // read this file into InputStream
+            inputStream = new FileInputStream(input);
+
+            // write the inputStream to a FileOutputStream
+            outputStream = new FileOutputStream(new File(output));
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+
+            System.out.println("Done!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    // outputStream.flush();
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public static void inputStreamSaveFile(InputStream inputStream, String output){
+
+        OutputStream outputStream = null;
+
+        try {
+
+            // write the inputStream to a FileOutputStream
+            outputStream = new FileOutputStream(new File(output));
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+
+            System.out.println("Done!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    // outputStream.flush();
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+}

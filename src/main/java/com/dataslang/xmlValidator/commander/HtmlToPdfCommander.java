0711e25af9c3c4698d3fc7c5db721c0e12f5d569
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2016-05-13 13:46:09 +0200

    changed param input, set up output as mandatory

--- src/main/java/com/dataslang/xmlValidator/commander/HtmlToPdfCommander.java
+++ src/main/java/com/dataslang/xmlValidator/commander/HtmlToPdfCommander.java
@@ -5,9 +5,9 @@ import com.beust.jcommander.Parameters;
 
 @Parameters(commandDescription = "turns HTML file into PDF")
 public class HtmlToPdfCommander {
-    @Parameter(names = "--html", required = true, description = "turns document into PDF")
+    @Parameter(names = {"-i", "--input"}, required = true, description = "turns document into PDF")
     public String input;
 
-    @Parameter(names = {"-o", "--output"}, description = "generate the PDF file into this destination")
+    @Parameter(names = {"-o", "--output"}, required = true, description = "generate the PDF file into this destination")
     public String output;
 }

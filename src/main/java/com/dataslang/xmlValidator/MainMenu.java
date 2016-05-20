7289c405ca14ea13e6b691aceae7b1b15e54ec0e
Tbaile <bailetti.tommaso@gmail.com>
2016-05-20 12:07:51 +0200

    adapted to previous commits

--- src/main/java/com/dataslang/xmlValidator/MainMenu.java
+++ src/main/java/com/dataslang/xmlValidator/MainMenu.java
@@ -1,36 +1,44 @@
 package com.dataslang.xmlValidator;
 
 import com.beust.jcommander.JCommander;
-import com.dataslang.xmlValidator.actions.HtmlEditor;
-import com.dataslang.xmlValidator.actions.PdfEditor;
-import com.dataslang.xmlValidator.actions.Transformator;
-import com.dataslang.xmlValidator.actions.Validator;
+import com.dataslang.xmlValidator.actions.HTMLUtil;
+import com.dataslang.xmlValidator.actions.PDFUtil;
+import com.dataslang.xmlValidator.actions.XMLUtil;
 import com.dataslang.xmlValidator.commander.*;
 
 public class MainMenu {
 
-    private static final String VALIDATE_OPTION = "validate";
-    private static final String TRANSFORM_OPTION = "transform";
-    private static final String META_OPTION = "meta";
-    private static final String HTML_OPTION = "html";
+    private static final String XML_VALIDATE_OPTION = "xmlvalidate";
+    private static final String XML_TRASFORM_OPTION = "xmltrasform";
+    private static final String HTML_TRASFORM_PDF_OPTION = "htmltrasform";
+    private static final String PDF_META_SET_OPTION = "pdfmetaset";
+    private static final String PDF_META_GET_OPTION = "pdfmetaget";
+    private static final String PDF_VALIDATE_OPTION = "pdfvalidate";
 
     public static void main(String[] args) {
         MainCommander com = new MainCommander();
-        ValCommander val = new ValCommander();
-        XsltCommander xsl = new XsltCommander();
-        MetaCommander dat = new MetaCommander();
-        HtmlToPdfCommander html = new HtmlToPdfCommander();
 
+        XMLValidateCommander xmlValidateCommander = new XMLValidateCommander();
+        XMLTrasformCommander xmlTrasformCommander = new XMLTrasformCommander();
+        HTMLTrasformCommander htmlTrasformCommander = new HTMLTrasformCommander();
+        PDFMetaSetCommander pdfMetaSetCommander = new PDFMetaSetCommander();
+        PDFMetaGetCommander pdfMetaGetCommander = new PDFMetaGetCommander();
+        PDFValidateCommander pdfValidateCommander = new PDFValidateCommander();
 
         JCommander jct = null;
 
         try {
+
             jct = new JCommander(com);
-            jct.addCommand(HTML_OPTION, html);
-            jct.addCommand(VALIDATE_OPTION, val);
-            jct.addCommand(TRANSFORM_OPTION, xsl);
-            jct.addCommand(META_OPTION, dat);
+            jct.addCommand(XML_VALIDATE_OPTION, xmlValidateCommander);
+            jct.addCommand(XML_TRASFORM_OPTION, xmlTrasformCommander);
+            jct.addCommand(HTML_TRASFORM_PDF_OPTION, htmlTrasformCommander);
+            jct.addCommand(PDF_META_SET_OPTION, pdfMetaSetCommander);
+            jct.addCommand(PDF_META_GET_OPTION, pdfMetaGetCommander);
+            jct.addCommand(PDF_VALIDATE_OPTION, pdfValidateCommander);
+
             jct.parse(args);
+
         } catch (Exception e) {
             if (com.debug) {
                 e.printStackTrace();
@@ -47,19 +55,27 @@ public class MainMenu {
         }
 
         try {
-            if (jct.getParsedCommand().equalsIgnoreCase(VALIDATE_OPTION)) {
-                Validator validator = new Validator(val.xml, val.xsd);
-                validator.validate();
-            } else if (jct.getParsedCommand().equalsIgnoreCase(TRANSFORM_OPTION)) {
-                Transformator transformator = new Transformator(xsl.xml, xsl.xslt, xsl.output);
-                transformator.transformate();
-            } else if (jct.getParsedCommand().equalsIgnoreCase(META_OPTION)) {
-                PdfEditor pdfEditor = new PdfEditor(dat.pdf, dat.output, dat.meta, dat.validate);
-                pdfEditor.meta();
-            }else if (jct.getParsedCommand().equalsIgnoreCase(HTML_OPTION)){
-                HtmlEditor htmlEditor = new HtmlEditor(html.input, html.output);
-                htmlEditor.transform();
+
+            if(jct.getParsedCommand().equalsIgnoreCase(XML_VALIDATE_OPTION)){
+                XMLUtil xmlUtil = new XMLUtil(xmlValidateCommander.xml, xmlValidateCommander.xsd);
+                xmlUtil.validate();
+            }else if(jct.getParsedCommand().equalsIgnoreCase(XML_TRASFORM_OPTION)){
+                XMLUtil xmlUtil = new XMLUtil(xmlTrasformCommander.xml, xmlTrasformCommander.xslt, xmlTrasformCommander.output);
+                xmlUtil.transformate();
+            }else if(jct.getParsedCommand().equalsIgnoreCase(HTML_TRASFORM_PDF_OPTION)){
+                HTMLUtil htmlUtil = new HTMLUtil(htmlTrasformCommander.input, htmlTrasformCommander.output);
+                htmlUtil.transform();
+            }else if(jct.getParsedCommand().equalsIgnoreCase(PDF_META_SET_OPTION)){
+                PDFUtil pdfUtil = new PDFUtil(pdfMetaSetCommander.pdf, pdfMetaSetCommander.xml, pdfMetaSetCommander.output);
+                pdfUtil.setMeta();
+            }else if(jct.getParsedCommand().equalsIgnoreCase(PDF_META_GET_OPTION)){
+                PDFUtil pdfUtil = new PDFUtil(pdfMetaGetCommander.pdf, null, pdfMetaGetCommander.output);
+                pdfUtil.getMeta();
+            }else if(jct.getParsedCommand().equalsIgnoreCase(PDF_VALIDATE_OPTION)){
+                PDFUtil pdfUtil = new PDFUtil(pdfValidateCommander.pdf, null, null);
+                pdfUtil.validatePDF();
             }
+
         } catch (NullPointerException e) {
             jct.usage();
         } catch (Exception e){
